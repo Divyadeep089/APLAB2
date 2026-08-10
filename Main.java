97c2ac91fdb8ef49632718ce426337d06aaf8282
Divya Deep Singh <241b089@juetguna.in>
2026-08-10 11:15:02 +0530

    second commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    Add a1 = new Add();
+	    int result = a1.sum(5,6);
+		System.out.println(result);
+	}
+}
